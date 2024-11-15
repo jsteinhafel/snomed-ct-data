9ed02b1490d3d0c4a5a485e5e18037b52e35e9c3
Patrick Richards <prichards@tinkarbuild.com>
2024-11-15 12:02:07 -0500

    iterating through description.txt and extracting values enhancement.

--- snomed-ct-integration/src/test/java/dev/ikm/tinkar/snomedct/integration/DescriptionSemanticIT.java
+++ snomed-ct-integration/src/test/java/dev/ikm/tinkar/snomedct/integration/DescriptionSemanticIT.java
@@ -7,11 +7,7 @@ import dev.ikm.tinkar.common.service.ServiceKeys;
 import dev.ikm.tinkar.common.service.ServiceProperties;
 import dev.ikm.tinkar.component.Component;
 import dev.ikm.tinkar.coordinate.Calculators;
-import dev.ikm.tinkar.coordinate.stamp.StampCoordinateRecord;
-import dev.ikm.tinkar.coordinate.stamp.StampPositionRecord;
 import dev.ikm.tinkar.coordinate.stamp.StateSet;
-import dev.ikm.tinkar.coordinate.stamp.StampCoordinateRecord;
-import dev.ikm.tinkar.coordinate.stamp.StampPositionRecord;
 import dev.ikm.tinkar.coordinate.stamp.calculator.Latest;
 import dev.ikm.tinkar.coordinate.stamp.calculator.StampCalculator;
 import dev.ikm.tinkar.entity.Entity;
@@ -20,19 +16,21 @@ import dev.ikm.tinkar.entity.EntityVersion;
 import dev.ikm.tinkar.entity.PatternEntityVersion;
 import dev.ikm.tinkar.entity.SemanticEntityVersion;
 import dev.ikm.tinkar.terms.EntityProxy;
-import dev.ikm.tinkar.terms.EntityProxy;
 import dev.ikm.tinkar.terms.TinkarTerm;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.UUID;
 import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.concurrent.atomic.AtomicReference;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
 
 public class DescriptionSemanticIT {
 
@@ -49,18 +47,49 @@ public class DescriptionSemanticIT {
     public static void shutdown() {
         PrimitiveData.stop();
     }
+    @Test
+    public void testDescriptionSemantics()throws IOException {
+        String sourceFilePath ="src/test/resources/snomedct_descriptions_sample.txt";
+
+        try(BufferedReader br = new BufferedReader(new FileReader(sourceFilePath))){
+            String line;
+            while((line = br.readLine()) != null){
+                if (line.startsWith("id")) continue;
+                String[] columns = line.split("\\t");
+
+                //pass these args in assertion method
+                long effectiveTimeToLong=dateStringToEpochMillis(columns[1]);
+                StateSet descriptionStatus = Integer.parseInt(columns[2]) == 1 ? StateSet.ACTIVE : StateSet.INACTIVE;
+                EntityProxy.Concept descriptionType = SnomedUtility.getDescriptionType(columns[6]);
+                String term =columns[7];
+                EntityProxy.Concept caseSensitivityConcept = SnomedUtility.getDescriptionCaseSignificanceConcept(columns[8]);
+
+            }
+        }
+    }
+
+    private long dateStringToEpochMillis(String dateString) {
+        long epochMillis;
+        try {
+            epochMillis = new SimpleDateFormat("yyyyMMdd").parse(dateString).getTime();
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+        return epochMillis;
+    }
 
     @Test
     public void testSingleSynonym() {
         String expectedSynonym = "Tumor of pancreas";
         UUID regularNameDescriptionId = UUID.fromString("d7da4d59-8bdf-38cf-b863-c657c35a284e");
 
-        Entity<EntityVersion> entity = EntityService.get().getEntityFast(regularNameDescriptionId);
+
+        Entity<EntityVersion> cldEntity = EntityService.get().getEntityFast(regularNameDescriptionId);
         StampCalculator stampCalc = Calculators.Stamp.DevelopmentLatestActiveOnly();
         PatternEntityVersion latestDescriptionPattern = (PatternEntityVersion) stampCalc.latest(TinkarTerm.DESCRIPTION_PATTERN).get();
 
         AtomicBoolean matchFound = new AtomicBoolean(false);
-        EntityService.get().forEachSemanticForComponentOfPattern(entity.nid(), TinkarTerm.DESCRIPTION_PATTERN.nid(), (descriptionSemantic) -> {
+        EntityService.get().forEachSemanticForComponentOfPattern(cldEntity.nid(), TinkarTerm.DESCRIPTION_PATTERN.nid(), (descriptionSemantic) -> {
 
             Latest<SemanticEntityVersion> latestDescriptionSemantic = stampCalc.latest(descriptionSemantic);
             Component descriptionType = latestDescriptionPattern.getFieldWithMeaning(TinkarTerm.DESCRIPTION_TYPE, latestDescriptionSemantic.get());
@@ -74,70 +103,4 @@ public class DescriptionSemanticIT {
         });
         assertTrue(matchFound.get(), "No synonym found: " + expectedSynonym);
     }
-
-    /**
-     * Test FQN Term value.
-     * @result Term is validated against a valid DataSet.
-     */
-    @Test
-    public void testFQN() {
-        // Given
-        String expectedTermFqn = "Erythema gyratum repens (disorder)";
-        String actualTermFqn = "";
-        String actualUUID = "ba8a3539-31fa-307a-895c-37401f0aea78";
-
-        UUID diseaseId = UUID.fromString(actualUUID);
-        Entity<EntityVersion> entity = EntityService.get().getEntityFast(diseaseId);
-        StampCalculator stampCalc = Calculators.Stamp.DevelopmentLatestActiveOnly();
-
-        // When
-        PatternEntityVersion latestDescriptionPattern = (PatternEntityVersion) stampCalc.latest(TinkarTerm.DESCRIPTION_PATTERN).get();
-        AtomicReference<SemanticEntityVersion> fqnVersion = new AtomicReference<>();
-        EntityService.get().forEachSemanticForComponentOfPattern(entity.nid(), TinkarTerm.DESCRIPTION_PATTERN.nid(), (descriptionSemantic) -> {
-            Latest<SemanticEntityVersion> latestDescriptionSemantic = stampCalc.latest(descriptionSemantic);
-            Component descriptionType = latestDescriptionPattern.getFieldWithMeaning(TinkarTerm.DESCRIPTION_TYPE, latestDescriptionSemantic.get());
-            if (PublicId.equals(descriptionType.publicId(), TinkarTerm.FULLY_QUALIFIED_NAME_DESCRIPTION_TYPE)) {
-                fqnVersion.set(latestDescriptionSemantic.get());
-            }
-        });
-
-        actualTermFqn = latestDescriptionPattern.getFieldWithMeaning(TinkarTerm.TEXT_FOR_DESCRIPTION, fqnVersion.get());
-
-        // Then
-        assertEquals(expectedTermFqn, actualTermFqn, "Message: Assert Term Values");
-    }
-
-    /**
-     * Assert Description
-     *
-     * @param term
-     * @param nameType
-     * @param caseSensitive
-     * @param effectiveDate
-     * @param activeFlag
-     */
-    private void assertDescription(String term, EntityProxy.Concept nameType, EntityProxy.Concept caseSensitive, long effectiveDate, StateSet activeFlag) {
-        // Given
-        String actualTermFqn = "";
-        StampPositionRecord stampPosition = StampPositionRecord.make(effectiveDate, TinkarTerm.DEVELOPMENT_PATH.nid());
-        StampCalculator stampCalc = StampCoordinateRecord.make(activeFlag, stampPosition).stampCalculator();
-
-        // When
-        PatternEntityVersion latestDescriptionPattern = (PatternEntityVersion) stampCalc.latest(TinkarTerm.DESCRIPTION_PATTERN).get();
-        AtomicReference<String> actualFqn = new AtomicReference<>();
-        EntityService.get().forEachSemanticOfPattern(latestDescriptionPattern.nid(), (semanticVersion) -> {
-            Latest<SemanticEntityVersion> latestDescriptionSemantic = stampCalc.latest(semanticVersion);
-            if (latestDescriptionSemantic.isPresent()) {
-                Component descriptionType = latestDescriptionPattern.getFieldWithMeaning(TinkarTerm.DESCRIPTION_TYPE, latestDescriptionSemantic.get());
-                if (PublicId.equals(descriptionType.publicId(), nameType)) {
-                    String text = latestDescriptionPattern.getFieldWithMeaning(TinkarTerm.TEXT_FOR_DESCRIPTION, latestDescriptionSemantic.get());
-                    if (text.contains(term)) {
-                        actualFqn.set(text);
-                    }
-                }
-            }
-        });
-
-    }
-
 }
---
+++ snomed-ct-integration/src/test/java/dev/ikm/tinkar/snomedct/integration/SnomedUtility.java
@@ -0,0 +1,276 @@
+/*
+ * Copyright © 2015 IKM (support@ikm.dev)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package dev.ikm.tinkar.snomedct.integration;
+
+
+import dev.ikm.tinkar.common.id.PublicIds;
+import dev.ikm.tinkar.common.service.CachingService;
+import dev.ikm.tinkar.common.service.PrimitiveData;
+import dev.ikm.tinkar.common.service.ServiceKeys;
+import dev.ikm.tinkar.common.service.ServiceProperties;
+import dev.ikm.tinkar.common.util.io.FileUtil;
+import dev.ikm.tinkar.common.util.uuid.UuidUtil;
+import dev.ikm.tinkar.entity.Entity;
+import dev.ikm.tinkar.entity.EntityService;
+import dev.ikm.tinkar.entity.EntityVersion;
+import dev.ikm.tinkar.entity.export.ExportEntitiesController;
+import dev.ikm.tinkar.starterdata.UUIDUtility;
+import dev.ikm.tinkar.terms.EntityProxy;
+import dev.ikm.tinkar.terms.EntityProxy.Concept;
+import dev.ikm.tinkar.terms.TinkarTerm;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+import java.util.concurrent.ExecutionException;
+import java.util.regex.MatchResult;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+public class SnomedUtility {
+
+    private static final Logger LOG = LoggerFactory.getLogger(SnomedUtility.class.getSimpleName());
+    private final UUIDUtility UUID_UTILITY;
+    private final List<Entity<? extends EntityVersion>> STAMP_LIST = new ArrayList<>();
+
+    /**
+     * constructor for SnomedUtility
+     * @param uuidUtility used to create uuid's
+     */
+    public SnomedUtility(UUIDUtility uuidUtility) {
+        this.UUID_UTILITY = uuidUtility;
+    }
+
+    /**
+     * creating a stamp
+     * @param status String that represents active or inactive
+     * @param effectiveTime String that represents dates in yyyyMMdd format
+     * @param authorConcept {@link dev.ikm.tinkar.terms.EntityProxy.Concept}
+     * @param moduleId String to represent a module
+     * @param pathConcept {@link dev.ikm.tinkar.terms.EntityProxy.Concept}
+     * @return new stamp entity
+     */
+
+
+    /**
+     * writes stamps
+     */
+    public void writeSTAMPs() {
+        Set<Entity<? extends EntityVersion>> stampSet = new HashSet<>(this.STAMP_LIST);
+        stampSet.forEach(stampEntity -> EntityService.get().putEntity(stampEntity));
+    }
+
+    /**
+     * taking time stamp and making it an epoch
+     * @param effectiveTime String representation dates in yyyyMMdd format
+     * @return long value of epochTime
+     */
+    public static long snomedTimestampToEpochSeconds(String effectiveTime) {
+        long epochTime;
+        try {
+            epochTime = new SimpleDateFormat("yyyyMMdd").parse(effectiveTime).getTime();
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+        return epochTime;
+    }
+
+    /**
+     * transforms languageCode in concept
+     * @param languageCode String representation of english or spanish
+     * @return language concept
+     */
+    public static EntityProxy.Concept getLanguageConcept(String languageCode){
+        EntityProxy.Concept languageConcept = null;
+        switch (languageCode) {
+            case "en" -> languageConcept = TinkarTerm.ENGLISH_LANGUAGE;
+            case "es" -> languageConcept = TinkarTerm.SPANISH_LANGUAGE;
+            default -> throw new RuntimeException("UNRECOGNIZED LANGUAGE CODE");
+        }
+        return languageConcept;
+    }
+
+    /**
+     * transforms caseSensitivity code into concept
+     * @param caseSensitivityCode represents case sensitivity of a description
+     * @return case sensitivity concept
+     */
+    public static EntityProxy.Concept getDescriptionCaseSignificanceConcept(String caseSensitivityCode){
+        EntityProxy.Concept caseSensitivityConcept = null;
+        switch (caseSensitivityCode) {
+            case "900000000000448009" -> caseSensitivityConcept = TinkarTerm.DESCRIPTION_NOT_CASE_SENSITIVE;
+            case "900000000000017005" -> caseSensitivityConcept = TinkarTerm.DESCRIPTION_CASE_SENSITIVE;
+            case "900000000000020002" -> caseSensitivityConcept = TinkarTerm.DESCRIPTION_INITIAL_CHARACTER_CASE_SENSITIVE;
+            default -> throw new RuntimeException("UNRECOGNIZED CASE SENSITIVITY CODE");
+        }
+        return caseSensitivityConcept;
+    }
+
+    /**
+     * transform descriptionType into concept
+     * @param descriptionTypeCode String representation the type of descriptions
+     * @return description type concept
+     */
+     public static EntityProxy.Concept getDescriptionType(String descriptionTypeCode){
+        EntityProxy.Concept descriptionTypeConcept = null;
+        switch (descriptionTypeCode) {
+            case "900000000000550004" -> descriptionTypeConcept = TinkarTerm.DEFINITION_DESCRIPTION_TYPE;
+            case "900000000000003001" -> descriptionTypeConcept = TinkarTerm.FULLY_QUALIFIED_NAME_DESCRIPTION_TYPE;
+            case "900000000000013009" -> descriptionTypeConcept = TinkarTerm.REGULAR_NAME_DESCRIPTION_TYPE;
+            default -> throw new RuntimeException("UNRECOGNIZED DESCRIPTION TYPE CODE");
+        }
+        return descriptionTypeConcept;
+    }
+
+    /**
+     * assigns dialect pattern to TinkarTerm depending on dialectRefSetId
+     * @param dialectRefsetId GB or US dialect semantic
+     * @return dialect pattern
+     */
+    public static EntityProxy.Pattern getDialectPattern(String dialectRefsetId) {
+        EntityProxy.Pattern dialectPattern = null;
+        switch (dialectRefsetId) {
+            case "900000000000509007" -> dialectPattern = TinkarTerm.US_DIALECT_PATTERN;
+            case "900000000000508004" -> dialectPattern = TinkarTerm.GB_DIALECT_PATTERN;
+            default -> throw new RuntimeException("UNRECOGNIZED ACCEPTABILITY CODE");
+        }
+        return dialectPattern;
+    }
+
+    /**
+     * assigns acceptability code to TinkarTerm based on acceptabilityCode
+     * @param acceptabilityCode preferred or acceptable
+     * @return acceptability concept
+     */
+    public static EntityProxy.Concept getDialectAccceptability(String acceptabilityCode){
+        EntityProxy.Concept acceptabilityConcept = null;
+        switch (acceptabilityCode) {
+            case "900000000000548007" -> acceptabilityConcept = TinkarTerm.PREFERRED;
+            case "900000000000549004" -> acceptabilityConcept = TinkarTerm.ACCEPTABLE;
+            default -> throw new RuntimeException("UNRECOGNIZED ACCEPTABILITY CODE");
+        }
+        return acceptabilityConcept;
+    }
+
+    /**
+     * retrieves user concept
+     * @return the snomed author
+     */
+        public static EntityProxy.Concept getUserConcept(){
+        EntityProxy.Concept snomedAuthor = EntityProxy.Concept.make("IHTSDO SNOMED CT Author", new UUIDUtility().createUUID("IHTSDO SNOMED CT Author"));
+        return snomedAuthor;
+//        return TinkarTerm.USER;
+    }
+
+    /**
+     * retrieve a path concept
+     * @return TinkarTerm from Concept class
+     */
+    public static EntityProxy.Concept getPathConcept(){
+        return TinkarTerm.DEVELOPMENT_PATH;
+    }
+
+    /**
+     * retrieves identifier concept
+     * @return snomedIntId from Concept class
+     */
+    public static EntityProxy.Concept getIdentifierConcept(){
+        EntityProxy.Concept snomedIntID = EntityProxy.Concept.make(PublicIds.of(UuidUtil.fromSNOMED("900000000000294009")));
+        return snomedIntID;
+    }
+
+    public static void startDatabase(File dataStore, String controllerName) {
+        LOG.info("Starting database");
+        LOG.info("Loading data from " + dataStore.getAbsolutePath());
+        CachingService.clearAll();
+        ServiceProperties.set(ServiceKeys.DATA_STORE_ROOT, dataStore);
+        PrimitiveData.selectControllerByName(controllerName);
+        PrimitiveData.start();
+    }
+
+    public static void stopDatabase() {
+        PrimitiveData.stop();
+    }
+
+    public static void exportEntities(File exportFile) {
+        ExportEntitiesController exportEntitiesController = new ExportEntitiesController();
+        FileUtil.recursiveDelete(exportFile);
+        try {
+            exportEntitiesController.export(exportFile).get();
+        } catch (ExecutionException | InterruptedException ex){
+            ex.printStackTrace();
+        }
+    }
+
+    public List<Entity<? extends EntityVersion>> getStampList() {
+        return this.STAMP_LIST;
+    }
+
+    public Set<Entity<? extends EntityVersion>> getStampSet() {
+        return new HashSet<>(this.STAMP_LIST);
+    }
+
+    private static Pattern getIdPattern() {
+        // Expecting a Snomed identifier following a colon as shown below
+        // :609096000
+        // Pattern of at least one numeric character after colon
+        return Pattern.compile("(?<=:)([0-9]+)");
+    }
+
+    private static String idToPublicId(MatchResult id) {
+        String idString = id.group();
+        String publicIdString = PublicIds.of(UuidUtil.fromSNOMED(idString)).toString();
+        return publicIdString.replaceAll("\"", "");
+    }
+
+    private static Pattern getUrlPattern() {
+        // Expecting URL formatted as shown below
+        // <http://www.w3.org/2002/07/owl#>
+        // Pattern of characters between less-than and greater-than characters
+        return Pattern.compile("<[^>]+>");
+    }
+
+    private static String urlToPublicId(MatchResult id) {
+        String urlString = id.group();
+        // remove beginning less-than character and ending greater-than character
+        String idString = urlString.substring(1, urlString.length()-1);
+        // Generate UUID from URL bytes
+        String publicIdString = PublicIds.of(UUID.nameUUIDFromBytes(idString.getBytes())).toString();
+        return publicIdString.replaceAll("\"", "");
+    }
+
+    public static String owlAxiomIdsToPublicIds(String owlExpression) {
+        String publicIdOwlExpression = owlExpression;
+        // Replace URLs with a UUID representation
+        if (owlExpression.contains("<") & owlExpression.contains(">")) {
+            Matcher urlMatcher = getUrlPattern().matcher(publicIdOwlExpression);
+            publicIdOwlExpression = urlMatcher.replaceAll(SnomedUtility::urlToPublicId);
+        }
+        // Replace Snomed identifiers with a UUID representation
+        Matcher idMatcher = getIdPattern().matcher(publicIdOwlExpression);
+        publicIdOwlExpression = idMatcher.replaceAll(SnomedUtility::idToPublicId);
+        return publicIdOwlExpression;
+    }
+
+}
