aff6777763e6ad3317e0d754391f226b4bdf32db
Patrick Richards <prichards@tinkarbuild.com>
2024-11-15 13:52:31 -0500

    returning fqn method.

--- snomed-ct-integration/src/test/java/dev/ikm/tinkar/snomedct/integration/DescriptionSemanticIT.java
+++ snomed-ct-integration/src/test/java/dev/ikm/tinkar/snomedct/integration/DescriptionSemanticIT.java
@@ -7,6 +7,8 @@ import dev.ikm.tinkar.common.service.ServiceKeys;
 import dev.ikm.tinkar.common.service.ServiceProperties;
 import dev.ikm.tinkar.component.Component;
 import dev.ikm.tinkar.coordinate.Calculators;
+import dev.ikm.tinkar.coordinate.stamp.StampCoordinateRecord;
+import dev.ikm.tinkar.coordinate.stamp.StampPositionRecord;
 import dev.ikm.tinkar.coordinate.stamp.StateSet;
 import dev.ikm.tinkar.coordinate.stamp.calculator.Latest;
 import dev.ikm.tinkar.coordinate.stamp.calculator.StampCalculator;
@@ -28,6 +30,9 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.UUID;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicReference;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
@@ -95,4 +100,70 @@ public class DescriptionSemanticIT {
         });
         assertTrue(matchFound.get(), "No synonym found: " + expectedSynonym);
     }
+
+    /**
+     * Test FQN Term value.
+     * @result Term is validated against a valid DataSet.
+     */
+    @Test
+    public void testFQN() {
+        // Given
+        String expectedTermFqn = "Erythema gyratum repens (disorder)";
+        String actualTermFqn = "";
+        String actualUUID = "ba8a3539-31fa-307a-895c-37401f0aea78";
+
+        UUID diseaseId = UUID.fromString(actualUUID);
+        Entity<EntityVersion> entity = EntityService.get().getEntityFast(diseaseId);
+        StampCalculator stampCalc = Calculators.Stamp.DevelopmentLatestActiveOnly();
+
+        // When
+        PatternEntityVersion latestDescriptionPattern = (PatternEntityVersion) stampCalc.latest(TinkarTerm.DESCRIPTION_PATTERN).get();
+        AtomicReference<SemanticEntityVersion> fqnVersion = new AtomicReference<>();
+        EntityService.get().forEachSemanticForComponentOfPattern(entity.nid(), TinkarTerm.DESCRIPTION_PATTERN.nid(), (descriptionSemantic) -> {
+            Latest<SemanticEntityVersion> latestDescriptionSemantic = stampCalc.latest(descriptionSemantic);
+            Component descriptionType = latestDescriptionPattern.getFieldWithMeaning(TinkarTerm.DESCRIPTION_TYPE, latestDescriptionSemantic.get());
+            if (PublicId.equals(descriptionType.publicId(), TinkarTerm.FULLY_QUALIFIED_NAME_DESCRIPTION_TYPE)) {
+                fqnVersion.set(latestDescriptionSemantic.get());
+            }
+        });
+
+        actualTermFqn = latestDescriptionPattern.getFieldWithMeaning(TinkarTerm.TEXT_FOR_DESCRIPTION, fqnVersion.get());
+
+        // Then
+        assertEquals(expectedTermFqn, actualTermFqn, "Message: Assert Term Values");
+    }
+
+    /**
+     * Assert Description
+     *
+     * @param term
+     * @param nameType
+     * @param caseSensitive
+     * @param effectiveDate
+     * @param activeFlag
+     */
+    private void assertDescription(String term, EntityProxy.Concept nameType, EntityProxy.Concept caseSensitive, long effectiveDate, StateSet activeFlag) {
+        // Given
+        String actualTermFqn = "";
+        StampPositionRecord stampPosition = StampPositionRecord.make(effectiveDate, TinkarTerm.DEVELOPMENT_PATH.nid());
+        StampCalculator stampCalc = StampCoordinateRecord.make(activeFlag, stampPosition).stampCalculator();
+
+        // When
+        PatternEntityVersion latestDescriptionPattern = (PatternEntityVersion) stampCalc.latest(TinkarTerm.DESCRIPTION_PATTERN).get();
+        AtomicReference<String> actualFqn = new AtomicReference<>();
+        EntityService.get().forEachSemanticOfPattern(latestDescriptionPattern.nid(), (semanticVersion) -> {
+            Latest<SemanticEntityVersion> latestDescriptionSemantic = stampCalc.latest(semanticVersion);
+            if (latestDescriptionSemantic.isPresent()) {
+                Component descriptionType = latestDescriptionPattern.getFieldWithMeaning(TinkarTerm.DESCRIPTION_TYPE, latestDescriptionSemantic.get());
+                if (PublicId.equals(descriptionType.publicId(), nameType)) {
+                    String text = latestDescriptionPattern.getFieldWithMeaning(TinkarTerm.TEXT_FOR_DESCRIPTION, latestDescriptionSemantic.get());
+                    if (text.contains(term)) {
+                        actualFqn.set(text);
+                    }
+                }
+            }
+        });
+
+    }
+
 }
