36e017dbe5d20ad3da5c0dc068f1d76b9ba05913
Jonathan Romero <141253593+jonaromero86@users.noreply.github.com>
2024-11-20 20:30:34 -0500

    Added imports and uncomment code

--- snomed-ct-integration/src/test/java/dev/ikm/tinkar/snomedct/integration/DefinitionSemanticIT.java
+++ snomed-ct-integration/src/test/java/dev/ikm/tinkar/snomedct/integration/DefinitionSemanticIT.java
@@ -1,20 +1,27 @@
 package dev.ikm.tinkar.snomedct.integration;
 
+import dev.ikm.tinkar.common.id.PublicId;
 import dev.ikm.tinkar.common.service.CachingService;
 import dev.ikm.tinkar.common.service.PrimitiveData;
 import dev.ikm.tinkar.common.service.ServiceKeys;
 import dev.ikm.tinkar.common.service.ServiceProperties;
+import dev.ikm.tinkar.component.Component;
 import dev.ikm.tinkar.coordinate.Calculators;
+import dev.ikm.tinkar.coordinate.stamp.calculator.Latest;
 import dev.ikm.tinkar.coordinate.stamp.calculator.StampCalculator;
 import dev.ikm.tinkar.entity.Entity;
 import dev.ikm.tinkar.entity.EntityService;
 import dev.ikm.tinkar.entity.EntityVersion;
+import dev.ikm.tinkar.entity.PatternEntityVersion;
+import dev.ikm.tinkar.entity.SemanticEntityVersion;
+import dev.ikm.tinkar.terms.TinkarTerm;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.util.UUID;
+import java.util.concurrent.atomic.AtomicReference;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -51,17 +58,17 @@ public class DefinitionSemanticIT {
         StampCalculator stampCalc = Calculators.Stamp.DevelopmentLatestActiveOnly();
 
         // When
-//        PatternEntityVersion latestDescriptionPattern = (PatternEntityVersion) stampCalc.latest(TinkarTerm.DESCRIPTION_PATTERN).get(); //DYNAMIC_DEFINITION_DESCRIPTION
-//        AtomicReference<SemanticEntityVersion> matchFound = new AtomicReference<>();
-//        EntityService.get().forEachSemanticForComponentOfPattern(entity.nid(), TinkarTerm.DESCRIPTION_PATTERN.nid(), (descriptionSemantic) -> {
-//            Latest<SemanticEntityVersion> latestDefinitionSemantic = stampCalc.latest(descriptionSemantic);
-//            Component definitionType = latestDescriptionPattern.getFieldWithMeaning(TinkarTerm.DEFINITION_DESCRIPTION_TYPE, latestDefinitionSemantic.get());
-//            if (PublicId.equals(definitionType.publicId(), TinkarTerm.FULLY_QUALIFIED_NAME_DESCRIPTION_TYPE)) {
-//                matchFound.set(latestDefinitionSemantic.get());
-//            }
-//        });
-//
-//        actualTerm = latestDescriptionPattern.getFieldWithMeaning(TinkarTerm.TEXT_FOR_DESCRIPTION, matchFound.get());
+        PatternEntityVersion latestDescriptionPattern = (PatternEntityVersion) stampCalc.latest(TinkarTerm.DESCRIPTION_PATTERN).get(); //DYNAMIC_DEFINITION_DESCRIPTION
+        AtomicReference<SemanticEntityVersion> matchFound = new AtomicReference<>();
+        EntityService.get().forEachSemanticForComponentOfPattern(entity.nid(), TinkarTerm.DESCRIPTION_PATTERN.nid(), (descriptionSemantic) -> {
+            Latest<SemanticEntityVersion> latestDefinitionSemantic = stampCalc.latest(descriptionSemantic);
+            Component definitionType = latestDescriptionPattern.getFieldWithMeaning(TinkarTerm.DEFINITION_DESCRIPTION_TYPE, latestDefinitionSemantic.get());
+            if (PublicId.equals(definitionType.publicId(), TinkarTerm.FULLY_QUALIFIED_NAME_DESCRIPTION_TYPE)) {
+                matchFound.set(latestDefinitionSemantic.get());
+            }
+        });
+
+        actualTerm = latestDescriptionPattern.getFieldWithMeaning(TinkarTerm.TEXT_FOR_DESCRIPTION, matchFound.get());
 
         // Then
 //        assertEquals(expectedTerm, actualTerm, "Message: Assert Term Values");
