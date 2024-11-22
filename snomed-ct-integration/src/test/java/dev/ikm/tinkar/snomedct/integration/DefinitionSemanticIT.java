bbcaf3cb5085f981462fc39e0a429362e90bfe11
Jonathan Romero <141253593+jonaromero86@users.noreply.github.com>
2024-11-22 14:59:21 -0500

    Adding method assertDefinition

--- snomed-ct-integration/src/test/java/dev/ikm/tinkar/snomedct/integration/DefinitionSemanticIT.java
+++ snomed-ct-integration/src/test/java/dev/ikm/tinkar/snomedct/integration/DefinitionSemanticIT.java
@@ -9,6 +9,8 @@ import dev.ikm.tinkar.common.service.ServiceProperties;
 import dev.ikm.tinkar.common.util.uuid.UuidUtil;
 import dev.ikm.tinkar.component.Component;
 import dev.ikm.tinkar.coordinate.Calculators;
+import dev.ikm.tinkar.coordinate.stamp.StampCoordinateRecord;
+import dev.ikm.tinkar.coordinate.stamp.StampPositionRecord;
 import dev.ikm.tinkar.coordinate.stamp.StateSet;
 import dev.ikm.tinkar.coordinate.stamp.calculator.Latest;
 import dev.ikm.tinkar.coordinate.stamp.calculator.StampCalculator;
@@ -17,6 +19,8 @@ import dev.ikm.tinkar.entity.EntityService;
 import dev.ikm.tinkar.entity.EntityVersion;
 import dev.ikm.tinkar.entity.PatternEntityVersion;
 import dev.ikm.tinkar.entity.SemanticEntityVersion;
+import dev.ikm.tinkar.entity.SemanticRecord;
+import dev.ikm.tinkar.entity.SemanticVersionRecord;
 import dev.ikm.tinkar.terms.EntityProxy;
 import dev.ikm.tinkar.terms.TinkarTerm;
 import org.junit.jupiter.api.AfterAll;
@@ -24,8 +28,10 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.UUID;
 import java.util.concurrent.atomic.AtomicReference;
@@ -48,11 +54,20 @@ public class DefinitionSemanticIT {
         PrimitiveData.stop();
     }
 
+    /**
+     * Test Definition Semantics.
+     *
+     * @result Reads content from file and validates Definition of Semantics by calling private method assertDefinition().
+     */
     @Test
     public void testDefinitionSemantics() throws IOException {
-        String sourceFilePath = System.getProperty("user.home") + "/data/SnomedCT_InternationalRF2_PRODUCTION_20240201T120000Z/Full/Terminology/sct2_TextDefinition_Full-en_INT_20240201.txt";
-
-        try (BufferedReader br = new BufferedReader(new FileReader(sourceFilePath))) {
+        // Given
+        String sourceFilePath = System.getProperty("user.home") + "/data/SnomedCT_InternationalRF2_PRODUCTION_20241001T120000Z/Full/Terminology/sct2_TextDefinition_Full-en_INT_20241001.txt";
+        String errorFile = "target/failsafe-reports/descriptions_definitions_not_found.txt";
+        int notFound = 0;
+        // When
+        try (BufferedReader br = new BufferedReader(new FileReader(sourceFilePath));
+             BufferedWriter bw = new BufferedWriter(new FileWriter(errorFile))) {
             String line;
             while ((line = br.readLine()) != null) {
                 if (line.startsWith("id")) continue;
@@ -69,8 +84,16 @@ public class DefinitionSemanticIT {
                 String term = columns[7];
                 EntityProxy.Concept caseSensitivityConcept = SnomedUtility.getDescriptionCaseSignificanceConcept(columns[8]);
 
+                if (!assertDefinition(id, term, descriptionType, caseSensitivityConcept, effectiveTime, descriptionStatus)) {
+                    notFound++;
+                    bw.write(term + "\t" + id + "\t" + columns[1] +
+                            "\t" + (descriptionStatus.equals(StateSet.ACTIVE) ? "Active" : "Inactive") +
+                            "\t" + descriptionType.description() +
+                            "\t" + caseSensitivityConcept.description() + "\n");
+                }
             }
         }
+        assertEquals(0, notFound, "Unable to find " + notFound + " description definition semantics. Details written to " + errorFile);
     }
 
     /**
@@ -105,6 +128,27 @@ public class DefinitionSemanticIT {
         // Then
         assertEquals(expectedTerm, actualTerm, "Message: Assert Term Values");
     }
+
+    private boolean assertDefinition(UUID id, String term, EntityProxy.Concept nameType, EntityProxy.Concept caseSensitive, long effectiveDate, StateSet activeFlag) {
+        StampPositionRecord stampPosition = StampPositionRecord.make(effectiveDate, TinkarTerm.DEVELOPMENT_PATH.nid());
+        StampCalculator stampCalc = StampCoordinateRecord.make(activeFlag, stampPosition).stampCalculator();
+        SemanticRecord entity = EntityService.get().getEntityFast(id);
+
+        PatternEntityVersion latestDescriptionPattern = (PatternEntityVersion) stampCalc.latest(TinkarTerm.DESCRIPTION_PATTERN).get();
+        Latest<SemanticVersionRecord> latest = stampCalc.latest(entity);
+        if (latest.isPresent()) {
+            Component descriptionType = latestDescriptionPattern.getFieldWithMeaning(TinkarTerm.DESCRIPTION_TYPE, latest.get());
+            Component caseSensitivity = latestDescriptionPattern.getFieldWithMeaning(TinkarTerm.DESCRIPTION_CASE_SIGNIFICANCE, latest.get());
+            String text = latestDescriptionPattern.getFieldWithMeaning(TinkarTerm.TEXT_FOR_DESCRIPTION, latest.get());
+
+            if (PublicId.equals(descriptionType.publicId(), TinkarTerm.DEFINITION_DESCRIPTION_TYPE)) {
+                return descriptionType.equals(nameType) && caseSensitivity.equals(caseSensitive) && text.equals(term);
+            }
+
+        }
+        return false;
+    }
+
 }
 
 
