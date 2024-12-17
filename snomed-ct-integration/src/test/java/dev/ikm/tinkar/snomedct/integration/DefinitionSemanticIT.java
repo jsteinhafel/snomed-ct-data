1e5166be9b5a5d45a051a2d089ea07bf19d06e3b
Patrick Richards <prichards@tinkarbuild.com>
2024-12-17 17:10:29 -0500

    refactored abstract class

--- snomed-ct-integration/src/test/java/dev/ikm/tinkar/snomedct/integration/DefinitionSemanticIT.java
+++ snomed-ct-integration/src/test/java/dev/ikm/tinkar/snomedct/integration/DefinitionSemanticIT.java
@@ -16,12 +16,13 @@ import dev.ikm.tinkar.terms.EntityProxy;
 import dev.ikm.tinkar.terms.TinkarTerm;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 import java.util.UUID;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class DefinitionSemanticIT extends BaseIntegrationTest {
+public class DefinitionSemanticIT extends AbstractIntegrationTest {
 
     /**
      * Test Definition Semantics.
@@ -29,29 +30,28 @@ public class DefinitionSemanticIT extends BaseIntegrationTest {
      * @result Reads content from file and validates Definition of Semantics by calling private method assertDefinition().
      */
     @Test
-    public void testDefinitionSemantics() {
+    public void testDefinitionSemantics() throws IOException {
         String sourceFilePath = "../snomed-ct-origin/target/origin-sources/SnomedCT_ManagedServiceUS_PRODUCTION_US1000124_20240901T120000Z/Full/Terminology/sct2_TextDefinition_Full-en_US1000124_20240901.txt";
         String errorFile = "target/failsafe-reports/descriptions_definitions_not_found.txt";
         AtomicInteger notFound = new AtomicInteger(0);
 
-        processFileLines(sourceFilePath, columns -> {
-            UUID id = UuidT5Generator.get(UUID.fromString("3094dbd1-60cf-44a6-92e3-0bb32ca4d3de"), columns[0]); //Need hardcode ID on namespace for Snomed
-            long effectiveTime = SnomedUtility.snomedTimestampToEpochSeconds(columns[1]);
-            StateSet descriptionStatus = Integer.parseInt(columns[2]) == 1 ? StateSet.ACTIVE : StateSet.INACTIVE;
-            EntityProxy.Concept descriptionType = SnomedUtility.getDescriptionType(columns[6]);
-            String term = columns[7];
-            EntityProxy.Concept caseSensitivityConcept = SnomedUtility.getDescriptionCaseSignificanceConcept(columns[8]);
+        processFile(sourceFilePath, errorFile);
 
-            if (!assertDefinition(id, term, descriptionType, caseSensitivityConcept, effectiveTime, descriptionStatus)) {
-                logError(errorFile, "Definitions not found: " + notFound);
-            }
-            assertEquals(0, notFound.get(), "Unable to find " + notFound.get() + " description definition semantics. Details written to " + errorFile);
-        });
+        assertEquals(0, notFound.get(), "Unable to find " + notFound.get() + " description definition semantics. Details written to " + errorFile);
+
     }
 
-    private boolean assertDefinition(UUID id, String term, EntityProxy.Concept nameType, EntityProxy.Concept caseSensitive, long effectiveDate, StateSet activeFlag) {
-        StampPositionRecord stampPosition = StampPositionRecord.make(effectiveDate, TinkarTerm.DEVELOPMENT_PATH.nid());
-        StampCalculator stampCalc = StampCoordinateRecord.make(activeFlag, stampPosition).stampCalculator();
+    @Override
+    protected boolean assertLine(String[] columns) {
+        UUID id = UuidT5Generator.get(UUID.fromString("3094dbd1-60cf-44a6-92e3-0bb32ca4d3de"), columns[0]); //Need hardcode ID on namespace for Snomed
+        long effectiveTime = SnomedUtility.snomedTimestampToEpochSeconds(columns[1]);
+        StateSet descriptionStatus = Integer.parseInt(columns[2]) == 1 ? StateSet.ACTIVE : StateSet.INACTIVE;
+        EntityProxy.Concept descType = SnomedUtility.getDescriptionType(columns[6]);
+        String term = columns[7];
+        EntityProxy.Concept caseSensitivityConcept = SnomedUtility.getDescriptionCaseSignificanceConcept(columns[8]);
+
+        StampPositionRecord stampPosition = StampPositionRecord.make(effectiveTime, TinkarTerm.DEVELOPMENT_PATH.nid());
+        StampCalculator stampCalc = StampCoordinateRecord.make(descriptionStatus, stampPosition).stampCalculator();
         SemanticRecord entity = EntityService.get().getEntityFast(id);
 
         PatternEntityVersion latestDescriptionPattern = (PatternEntityVersion) stampCalc.latest(TinkarTerm.DESCRIPTION_PATTERN).get();
@@ -61,7 +61,7 @@ public class DefinitionSemanticIT extends BaseIntegrationTest {
             Component caseSensitivity = latestDescriptionPattern.getFieldWithMeaning(TinkarTerm.DESCRIPTION_CASE_SIGNIFICANCE, latest.get());
             String text = latestDescriptionPattern.getFieldWithMeaning(TinkarTerm.TEXT_FOR_DESCRIPTION, latest.get());
             if (PublicId.equals(descriptionType.publicId(), TinkarTerm.DEFINITION_DESCRIPTION_TYPE)) {
-                return descriptionType.equals(nameType) && caseSensitivity.equals(caseSensitive) && text.equals(term);
+                return descriptionType.equals(descType) && caseSensitivity.equals(caseSensitivityConcept) && text.equals(term);
             }
         }
         return false;
--- snomed-ct-integration/src/test/java/dev/ikm/tinkar/snomedct/integration/DescriptionSemanticIT.java
+++ snomed-ct-integration/src/test/java/dev/ikm/tinkar/snomedct/integration/DescriptionSemanticIT.java
@@ -15,12 +15,13 @@ import dev.ikm.tinkar.terms.EntityProxy;
 import dev.ikm.tinkar.terms.TinkarTerm;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 import java.util.UUID;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class DescriptionSemanticIT extends BaseIntegrationTest {
+public class DescriptionSemanticIT extends AbstractIntegrationTest {
 
     /**
      * Test Description Semantics.
@@ -28,39 +29,39 @@ public class DescriptionSemanticIT extends BaseIntegrationTest {
      * @result Reads content from file and validates Description of Semantics by calling private method assertDescription().
      */
     @Test
-    public void testDescriptionSemantics() {
+    public void testDescriptionSemantics() throws IOException {
         String sourceFilePath = "../snomed-ct-origin/target/origin-sources/SnomedCT_ManagedServiceUS_PRODUCTION_US1000124_20240901T120000Z/Full/Terminology/sct2_Description_Full-en_US1000124_20240901.txt";
         String errorFile = "target/failsafe-reports/descriptions_not_found.txt";
         AtomicInteger notFound = new AtomicInteger(0);
 
-        processFileLines(sourceFilePath, columns -> {
-            long effectiveTime = SnomedUtility.snomedTimestampToEpochSeconds(columns[1]);
-            StateSet descriptionStatus = Integer.parseInt(columns[2]) == 1 ? StateSet.ACTIVE : StateSet.INACTIVE;
-            EntityProxy.Concept descriptionType = SnomedUtility.getDescriptionType(columns[6]);
-            String term = columns[7];
-            EntityProxy.Concept caseSensitivityConcept = SnomedUtility.getDescriptionCaseSignificanceConcept(columns[8]);
-            UUID id = UuidT5Generator.get(UUID.fromString("3094dbd1-60cf-44a6-92e3-0bb32ca4d3de"), columns[0]); //Need hardcode ID on namespace for Snomed
+        processFile(sourceFilePath, errorFile);
 
-            if (!assertDescription(id, term, descriptionType, caseSensitivityConcept, effectiveTime, descriptionStatus)) {
-                logError(errorFile, "");
-            }
-        });
         assertEquals(0, notFound.get(), "Unable to find " + notFound.get() + " description semantics. Details written to " + errorFile);
     }
 
-    private boolean assertDescription(UUID id, String term, EntityProxy.Concept nameType, EntityProxy.Concept caseSensitive, long effectiveDate, StateSet activeFlag) {
-        StampPositionRecord stampPosition = StampPositionRecord.make(effectiveDate, TinkarTerm.DEVELOPMENT_PATH.nid());
-        StampCalculator stampCalc = StampCoordinateRecord.make(activeFlag, stampPosition).stampCalculator();
+
+    @Override
+    protected boolean assertLine(String[] columns) {
+        long effectiveTime = SnomedUtility.snomedTimestampToEpochSeconds(columns[1]);
+        StateSet descriptionStatus = Integer.parseInt(columns[2]) == 1 ? StateSet.ACTIVE : StateSet.INACTIVE;
+        EntityProxy.Concept descrType = SnomedUtility.getDescriptionType(columns[6]);
+        String term = columns[7];
+        EntityProxy.Concept caseSensitivityConcept = SnomedUtility.getDescriptionCaseSignificanceConcept(columns[8]);
+        UUID id = UuidT5Generator.get(UUID.fromString("3094dbd1-60cf-44a6-92e3-0bb32ca4d3de"), columns[0]); //Need hardcode ID on namespace for Snomed
+
+
+        StampPositionRecord stampPosition = StampPositionRecord.make(effectiveTime, TinkarTerm.DEVELOPMENT_PATH.nid());
+        StampCalculator stampCalc = StampCoordinateRecord.make(descriptionStatus, stampPosition).stampCalculator();
         SemanticRecord entity = EntityService.get().getEntityFast(id);
 
         PatternEntityVersion latestDescriptionPattern = (PatternEntityVersion) stampCalc.latest(TinkarTerm.DESCRIPTION_PATTERN).get();
         Latest<SemanticVersionRecord> latest = stampCalc.latest(entity);
         if (latest.isPresent()) {
             Component descriptionType = latestDescriptionPattern.getFieldWithMeaning(TinkarTerm.DESCRIPTION_TYPE, latest.get());
-            Component caseSensitivity = latestDescriptionPattern.getFieldWithMeaning(TinkarTerm.DESCRIPTION_CASE_SIGNIFICANCE, latest.get());
+            Component descCaseSensitivity = latestDescriptionPattern.getFieldWithMeaning(TinkarTerm.DESCRIPTION_CASE_SIGNIFICANCE, latest.get());
             String text = latestDescriptionPattern.getFieldWithMeaning(TinkarTerm.TEXT_FOR_DESCRIPTION, latest.get());
 
-            return descriptionType.equals(nameType) && caseSensitivity.equals(caseSensitive) && text.equals(term);
+            return descriptionType.equals(descrType) && descCaseSensitivity.equals(caseSensitivityConcept) && text.equals(term);
         }
         return false;
     }
