59d29c538c6695eb829108b7baaed36594dc173c
jsteinhafel <jsteinhafel@deloitte.com>
2025-01-14 06:34:24 -0800

    T5 UUID generation update

--- snomed-ct-integration/src/test/java/dev/ikm/tinkar/snomedct/integration/AbstractIntegrationTest.java
+++ snomed-ct-integration/src/test/java/dev/ikm/tinkar/snomedct/integration/AbstractIntegrationTest.java
@@ -1,5 +1,6 @@
 package dev.ikm.tinkar.snomedct.integration;
 
+import dev.ikm.maven.SnomedUtility;
 import dev.ikm.tinkar.common.service.CachingService;
 import dev.ikm.tinkar.common.service.PrimitiveData;
 import dev.ikm.tinkar.common.service.ServiceKeys;
@@ -94,8 +95,7 @@ public abstract class AbstractIntegrationTest {
     }
 
     protected UUID uuid(String id) {
-        return UuidUtil.fromSNOMED(id);
-//        return UuidT5Generator.get(UUID.fromString("3094dbd1-60cf-44a6-92e3-0bb32ca4d3de"), id);
+        return SnomedUtility.generateUUID(UUID.fromString("3094dbd1-60cf-44a6-92e3-0bb32ca4d3de"), id);
     }
 
     protected abstract boolean assertLine(String[] columns);
