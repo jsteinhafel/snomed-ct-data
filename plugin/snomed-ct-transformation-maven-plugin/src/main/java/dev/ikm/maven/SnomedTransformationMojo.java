4e0ebbd3c34f48ca0c8010860ee6c52f38ef3018
Jonathan Romero <141253593+jonaromero86@users.noreply.github.com>
2025-02-27 17:40:37 -0500

    Update the SnomedTransformationMojo.getTransformer method to validate the Identifier file type

--- plugin/snomed-ct-transformation-maven-plugin/src/main/java/dev/ikm/maven/SnomedTransformationMojo.java
+++ plugin/snomed-ct-transformation-maven-plugin/src/main/java/dev/ikm/maven/SnomedTransformationMojo.java
@@ -6,19 +6,17 @@ import dev.ikm.tinkar.common.service.ServiceKeys;
 import dev.ikm.tinkar.common.service.ServiceProperties;
 import dev.ikm.tinkar.composer.Composer;
 import dev.ikm.tinkar.entity.EntityService;
-
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
-
 import org.apache.maven.plugins.annotations.Parameter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Arrays;
@@ -62,19 +60,19 @@ public class SnomedTransformationMojo extends AbstractMojo {
 
     private String unzipRawData(String zipFilePath) throws IOException {
         File outputDirectory = new File(dataOutputPath);
-        try(ZipInputStream zis = new ZipInputStream(new FileInputStream(zipFilePath))) {
+        try (ZipInputStream zis = new ZipInputStream(new FileInputStream(zipFilePath))) {
             ZipEntry zipEntry;
             while ((zipEntry = zis.getNextEntry()) != null) {
                 File newFile = new File(outputDirectory, zipEntry.getName());
-                if(zipEntry.isDirectory()) {
+                if (zipEntry.isDirectory()) {
                     newFile.mkdirs();
                 } else {
                     new File(newFile.getParent()).mkdirs();
-                    try(FileOutputStream fos = new FileOutputStream(newFile)) {
+                    try (FileOutputStream fos = new FileOutputStream(newFile)) {
                         byte[] buffer = new byte[1024];
                         int len;
                         while ((len = zis.read(buffer)) > 0) {
-                            fos.write(buffer,0,len);
+                            fos.write(buffer, 0, len);
                         }
                     }
                 }
@@ -91,12 +89,12 @@ public class SnomedTransformationMojo extends AbstractMojo {
     }
 
     private static File searchTerminologyFolder(File dir) {
-        if (dir.isDirectory()){
+        if (dir.isDirectory()) {
             File[] files = dir.listFiles();
             if (files != null) {
                 for (File file : files) {
-                    if(file.isDirectory() && file.getName().equals("Terminology") &&
-                        file.getParentFile().getName().equals("Full")) {
+                    if (file.isDirectory() && file.getName().equals("Terminology") &&
+                            file.getParentFile().getName().equals("Full")) {
                         return file;
                     }
                     File found = searchTerminologyFolder(file);
@@ -110,7 +108,7 @@ public class SnomedTransformationMojo extends AbstractMojo {
     }
 
     private void validateInputDirectory(File inputFileOrDirectory) throws MojoExecutionException {
-        if(!inputFileOrDirectory.exists()){
+        if (!inputFileOrDirectory.exists()) {
             throw new RuntimeException("Invalid input directory or file. Directory or file does not exist");
         }
     }
@@ -118,10 +116,10 @@ public class SnomedTransformationMojo extends AbstractMojo {
     /**
      * Transforms each snomed file in a directory based on filename
      *
-     * @param datastore location of datastore to write entities to
+     * @param datastore            location of datastore to write entities to
      * @param inputFileOrDirectory directory containing snomed files
      */
-    public void transformFile(File datastore, File inputFileOrDirectory){
+    public void transformFile(File datastore, File inputFileOrDirectory) {
         LOG.info("########## Snomed Transformer Starting...");
         initializeDatastore(datastore);
 
@@ -137,15 +135,15 @@ public class SnomedTransformationMojo extends AbstractMojo {
         }
     }
 
-    private void initializeDatastore(File datastore){
+    private void initializeDatastore(File datastore) {
         CachingService.clearAll();
         ServiceProperties.set(ServiceKeys.DATA_STORE_ROOT, datastore);
         PrimitiveData.selectControllerByName(controllerName);
         PrimitiveData.start();
     }
 
-    private void processFilesFromInput(File inputFileOrDirectory, Composer composer){
-        if(inputFileOrDirectory.isDirectory()){
+    private void processFilesFromInput(File inputFileOrDirectory, Composer composer) {
+        if (inputFileOrDirectory.isDirectory()) {
             Arrays.stream(inputFileOrDirectory.listFiles())
                     .filter(file -> file.getName().endsWith(".txt"))
                     .forEach(file -> processIndividualFile(file, composer));
@@ -173,15 +171,17 @@ public class SnomedTransformationMojo extends AbstractMojo {
      * @param fileName File for Transformer match
      */
     private Transformer getTransformer(String fileName) {
-        if(fileName.contains("Concept")){
+        if (fileName.contains("Concept")) {
             return new ConceptTransformer(namespace);
-        } else if(fileName.contains("Definition")){
+        } else if (fileName.contains("Definition")) {
             return new DefinitionTransformer(namespace);
-        } else if(fileName.contains("Description")){
+        } else if (fileName.contains("Description")) {
             return new DescriptionTransformer(namespace);
-        } else if(fileName.contains("Language")){
+        } else if (fileName.contains("Language")) {
             return new LanguageTransformer(namespace);
-        } else if(fileName.contains("OWLExpression")){
+        } else if (fileName.contains("Identifier")) {
+            return new IdentifierTransformer(namespace);
+        } else if (fileName.contains("OWLExpression")) {
             return new AxiomSyntaxTransformer(namespace);
         }
         return null;
