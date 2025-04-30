a47b817401bfbe3dd94dd641da494411476f21f0
Jonathan Romero <141253593+jonaromero86@users.noreply.github.com>
2025-04-30 15:28:43 -0400

    Adding ElkSnomedDataBuilderTestBaseIT from tinkar-core

--- snomed-ct-integration/src/test/java/dev/ikm/tinkar/snomedct/integration/ElkSnomedCompareTestBaseIT.java
+++ snomed-ct-integration/src/test/java/dev/ikm/tinkar/snomedct/integration/ElkSnomedCompareTestBaseIT.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright © 2015 Integrated Knowledge Management (support@ikm.dev)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package dev.ikm.tinkar.snomedct.integration;
 
 import dev.ikm.elk.snomed.ConceptComparer;
@@ -89,12 +104,6 @@ public abstract class ElkSnomedCompareTestBaseIT extends AbstractIntegrationTest
 
 	@Test
 	public void compare() throws Exception {
-//		assumeTrue(Files.exists(axioms_file), "No file: " + axioms_file);
-//		assumeTrue(Files.exists(rels_file), "No file: " + rels_file);
-//		LOG.info("Files exist");
-//		LOG.info("\t" + axioms_file);
-//		LOG.info("\t" + rels_file);
-
 		String sourceFilePath = "../snomed-ct-origin/target/origin-sources";
 		Path axioms_file = Paths.get(findFilePath(sourceFilePath, "sct2_sRefset_OWLExpressionSnapshot_"));
 		Path rels_file = Paths.get(findFilePath(sourceFilePath, "sct2_Relationship_Snapshot_"));
